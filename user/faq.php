82c1676b773ddd2136d98a58853badc037164f19
StudentChat <mail@studentchat.ru>
2022-05-25 16:17:23 +0300

    упрощен хедер

--- user/faq.php
+++ user/faq.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>О нас</title>
-    <link rel="stylesheet" href="../css/style.css" />
-    <link rel="icon" href="../img/Logo-light.svg" />
-  </head>
-  <body>
-    <header class="header center">
-      <a class="logo" href="wall.php"
-        ><img class="logo" src="../img/Logo-image.svg" alt="Логотип"
-      /></a>
-      <div class="btns">
-        <a class="btn btnLink" href="wall.php">Стена</a>
-        <a class="btn btnLink" href="timetable.php">Расписание</a>
-        <a class="btn btnLink" href="chat.php">Чат</a>
-        <a class="btn btnLink" href="vacancy.php">Вакансии</a>
-        <a class="btn btnLink" href="faq.php">О нас</a>
-      </div>
-      <div class="btns">
-        <a href="addPost.php"
-          ><img class="headerIcon" src="../img/add.svg" alt="Добавить пост"
-        /></a>
-        <a href="profile.php"
-          ><img class="headerIcon" src="../img/profile.svg" alt="Профиль"
-        /></a>
-        <a href="setting.php">
-          <img class="headerIcon" src="../img/setting.svg" alt="Настройки"
-        /></a>
-        <!-- сделать выход через js -->
-        <a href="../index.php">
-          <img class="headerIcon" src="../img/log out.svg" alt="Выход"
-        /></a>
-      </div>
-    </header>
+<?php
+require_once("../modules/headerUser.php")
+?>
 
     <section class="container">
       <h1 class="titleFAQ">Документация</h1>
